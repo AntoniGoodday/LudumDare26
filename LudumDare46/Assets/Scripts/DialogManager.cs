0a30f0ef2b5d82f69080e6ab7a0e4635c77e936e
mrrollingcircle <63915346+mrrollingcircle@users.noreply.github.com>
2020-04-18 23:33:16 +0100

    Full dialog system
    All the dialog for the 1 scenario is complete. With text, choices, etc. Have to implement when a scene is done to go back to the map

--- LudumDare46/Assets/Scripts/DialogManager.cs
+++ LudumDare46/Assets/Scripts/DialogManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
@@ -10,8 +11,10 @@ public class DialogManager : MonoBehaviour
     public int index;
     public float baseTypingSpeed;
     public float typingSpeed;
-    public GameObject parentNPC;
+    public GameObject proceedButton;
     public bool currentlyTyping = true;
+    public int choiceNumber = 0;
+    public bool makingChoice = false;
 
 
     void Start()
@@ -36,48 +39,60 @@ public class DialogManager : MonoBehaviour
         currentlyTyping = false;
     }
 
-
-    //
     void LateUpdate()
     {
-        //repeatedly clear the text if not in conversation if there is text
-       // if (parentNPC.GetComponent<NPCBasic>().chatting == false) { textDisplay.text = ""; textDisplay.enabled = false; }
-
-        if (currentlyTyping == false && index == 0)
+        //make proceed button visbile if text has stopped typing, invisible when text is still typing
+        if (currentlyTyping == false && textDisplay.text == sentences[index])
         {
-           // textDisplay.text = "";
-            //currentlyTyping = true;
-            //StartCoroutine(Typing());
+            proceedButton.GetComponent<RawImage>().enabled = true;
         }
-
-
-        //continue dialog, make sure there actually is more conversation too
-        if (Input.GetButtonDown("Fire1") && currentlyTyping == false && index <= sentences.Length - 1 && textDisplay.text == sentences[index])
+        if (currentlyTyping == true || makingChoice == true)
         {
-            //last index is blank so it looks like the conversation is finished
-            //restart convo
-            index ++;
-            textDisplay.text = "";
-            currentlyTyping = true;
-            StartCoroutine(Typing());
-
-
+            proceedButton.GetComponent<RawImage>().enabled = false;
         }
 
-        else
+        if (makingChoice == false)
+        { 
+            //this starts up the typing again after making a button choice
+            if (currentlyTyping == false && textDisplay.text != sentences[index])
+            {
+                textDisplay.text = "";
+                currentlyTyping = true;
+                StartCoroutine(Typing());
+            }
 
-        //skip text if press left click while its still typing
-        if (Input.GetButtonDown("Fire1") && currentlyTyping == true)
+            //continue dialog, make sure there actually is more conversation too
+            if (Input.GetButtonDown("Fire1") && currentlyTyping == false && index <= sentences.Length - 1 && textDisplay.text == sentences[index])
+            {
+                //last index is blank so it looks like the conversation is finished
+                index ++;
+                textDisplay.text = "";
+                currentlyTyping = true;
+                StartCoroutine(Typing());
+            }
+
+            else
+
+            //skip text if press left click while its still typing
+            if (Input.GetButtonDown("Fire1") && currentlyTyping == true)
+            {
+                StopCoroutine(Typing());
+                textDisplay.text = sentences[index];
+                currentlyTyping = false;
+            }
+        }
+        //ending dialogue and returning to map screen. When you want dialogue to end, make the index ahead of the current one say "End"
+        if (Input.GetButtonDown("Fire1") && currentlyTyping == false && sentences[index + 1] == "End")
         {
-            StopCoroutine(Typing());
-            textDisplay.text = sentences[index];
-            currentlyTyping = false;
+            Debug.Log("Return to map");
         }
 
-        if (Input.GetButtonUp("Fire1"))
+            //the game will now a choice is active when the string after the current string in the array is null with no text
+            if (currentlyTyping == false && textDisplay.text == sentences[index] && sentences[index + 1] == "")
         {
-            //typingSpeed = baseTypingSpeed;
+            Debug.Log("Choice");
+            makingChoice = true;
         }
 
-        }
+    }
 }
