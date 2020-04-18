4ab3587d043debc2bb844da8406d4f7b46e3a6a6
AntoniGoodday <32736437+AntoniGoodday@users.noreply.github.com>
2020-04-18 19:18:56 +0100

    Basic menus + quit with escape anytime

---
+++ LudumDare46/Assets/Scripts/MenuScipts/QuitWithEscape.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitWithEscape : MonoBehaviour
+{
+    public static QuitWithEscape persistentQuit;
+    // Update is called once per frame
+    private void Awake()
+    {
+        if(!persistentQuit)
+        {
+            persistentQuit = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
